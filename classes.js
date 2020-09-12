6d9e6a00cc186736df716a56cebc15cf71c08095
Michał Jagielski <michal@jagielski.net>
2020-09-12 15:47:16 +0200

    Introduce ProductLevelInterestGroupAd

--- classes.js
+++ classes.js
@@ -8,18 +8,26 @@ export class ContextualBidResponse {
 
 export class Ad {
   constructor (id, iframeContent, type, adPartner) {
-    this.id = id // Id of this ad. Should be unique globally.
+    this.id = id // Id of this ad. Should be unique for ad partner (ad may be overwritten on collision).
     this.iframeContent = iframeContent // Proper content of ad.
-    this.type = type // Type of an ad. Now can be either "contextual" or "interest-group"
+    this.type = type // Type of an ad. Now can be either "contextual" or "interest-group".
     this.adPartner = adPartner // Partner that generated this ad.
   }
 }
 
 export class InterestGroupAd extends Ad {
-  constructor (id, iframeContent, interestGroupSignals, bidFunctionSrc, adPartner) {
+  constructor (id, groupName, iframeContent, interestGroupSignals, bidFunctionSrc, adPartner) {
     super(id, iframeContent, 'interest-group', adPartner)
+    this.groupName = groupName // interest group for which ad is intended
     this.interestGroupSignals = interestGroupSignals // Interest-group-related object that further will be propagated to bid function.
     this.bidFunctionSrc = bidFunctionSrc // Address to a function that consumes context signals with interestGroupSignals and returns bidValue.
+  }
+}
+
+export class ProductLevelInterestGroupAd extends InterestGroupAd {
+  constructor (id, groupName, iframeContent, interestGroupSignals, bidFunctionSrc, productsCount, adPartner) {
+    super(id, groupName, iframeContent, interestGroupSignals, bidFunctionSrc, adPartner)
+    this.productsCount = productsCount // States how many products have to be provided to ad to render it.
   }
 }
 
